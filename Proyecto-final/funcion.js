b8bcec1637a026fe9e87fed58ee21fa6be9cbdba
Nicolas Dorigoni <dorigoni.nicolas@yahoo.com>
2023-02-27 14:07:12 -0300

    subo proyecto

---
+++ Proyecto-final/funcion.js
@@ -0,0 +1,6 @@
+window.alert("Bienvenidos a mi Cv");
+
+
+document.getElementById("boton").onclick = function () {
+    document.getElementById("sobremitexto").innerHTML = "Deseo formar parte de una organización donde pueda desarrollar mis conocimientos y herramientas adquiridas. Me considero una persona responsable, proactiva, empática, conciliadora, de rápido aprendizaje y capaz de adaptarme a los diversos cambios que puedan llegar a presentarse. A su vez poseo el interés de seguir aprendiendo y perfeccionándome dentro de un equipo de trabajo dinámico, con objetivos claros y desafiantes, buscando crecer dentro de la organización. ";
+}
